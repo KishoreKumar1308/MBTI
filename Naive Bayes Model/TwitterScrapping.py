92c1e82da1e97d31888e9740c260609a76399b83
Kishore Kumar R <kishorekumar08072002@gmail.com>
2021-04-11 10:17:08 +0530

    Naive Bayes

---
+++ Naive Bayes Model/TwitterScrapping.py
@@ -0,0 +1,36 @@
+# Using Snscrape to get last 50 tweets of the specified user
+
+import numpy as np
+import pandas as pd
+import os
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
+import nltk
+
+nltk.download(["wordnet","punkt","stopwords"])
+es_stop_words = set(stopwords.words("english"))
+
+def scrape(user):
+    os.system(f"snscrape --jsonl --max-results 50 twitter-search 'from:{user}' > user-json-tweets.json")
+
+    user_json = pd.read_json("user-json-tweets.json",lines = True)
+    user_json.to_csv("user-tweets.csv")
+    df = pd.read_csv("user-tweets.csv")
+
+    tweet_list = []
+    for tweet in df["renderedContent"]:
+        tweet_list.append(tweet)
+
+    user_string = "|||".join(tweet_list)
+
+    clean_string = user_string.replace("+"," ").replace('.', " ").replace(',', ' ').replace(':', ' ').replace('@', '')
+    clean_string = re.sub(r'\|\|\|',r' ',clean_string)
+    clean_string = re.sub('http\S+','',clean_string)
+    clean_string = re.sub(r"@\S+","",clean_string)
+    clean_string = re.sub(r'[^a-zA-Z]',r' ',clean_string)
+    clean_string = re.sub(r' +',r' ',clean_string).lower()
+    
+    return clean_string
